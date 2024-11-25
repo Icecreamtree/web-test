6373c7c62639acfd7a79f3bf3d9a8c7c413d6ce0
Icecreamtree <159965155+Icecreamtree@users.noreply.github.com>
2024-11-24 19:04:44 -0800

    Create scripts.js

---
+++ scripts.js
@@ -0,0 +1,10 @@
+/* Place your JavaScript in this file */
+let btn = document.getElementById("Start") {
+    while btnn
+}
+let btn = document.getElementById("Stop") {
+    function replaceContent () {	
+    document.body.innerHTML = “CLICKED!!!”
+    }
+    btn.onclick = replaceContent;
+}
