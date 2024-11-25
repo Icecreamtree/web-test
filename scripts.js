6af718d632d17681a7b70c4351bc0e6037060786
Icecreamtree <159965155+Icecreamtree@users.noreply.github.com>
2024-11-24 19:12:49 -0800

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,10 +1,26 @@
 /* Place your JavaScript in this file */
-let btn = document.getElementById("Start") {
-    while btnn
-}
-let btn = document.getElementById("Stop") {
-    function replaceContent () {	
-    document.body.innerHTML = “CLICKED!!!”
-    }
-    btn.onclick = replaceContent;
+function myFunction() {
+  var at = document.getElementById("email").value.indexOf("@");
+  var age = document.getElementById("age").value;
+  var fname = document.getElementById("fname").value;
+  submitOK = "true";
+
+  if (fname.length > 10) {
+    alert("The name may have no more than 10 characters");
+    submitOK = "false";
+  } 
+
+  if (isNaN(age) || age < 1 || age > 100) {
+    alert("The age must be a number between 1 and 100");
+    submitOK = "false";
+  }
+
+  if (at == -1) {
+    alert("Not a valid e-mail!");
+    submitOK = "false";
+  }
+
+  if (submitOK == "false") {
+    return false;
+  }
 }
